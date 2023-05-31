0e4bf43deef73be60b20d0430f3b5c61d6b10737
James Grieve <jgrieve@ualberta.ca>
2023-05-30 20:14:55 -0600

    Fixed imports;

--- src/frontend/pages/subdirectory/example.js
+++ src/frontend/pages/subdirectory/example.js
@@ -1,8 +1,10 @@
-
+import {Typography} from '@mui/material';
 
 export default function Example(props) {
     return <>
-        <h1>Example</h1>
-        <p>Example page.</p>
+      <Typography variant="h1" align="center" component="h2">Example Sub-Page</Typography>
+      <Typography variant="body1" align="center" component="p">
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras porttitor laoreet lobortis. Sed imperdiet quis leo et tincidunt. Sed non ante nisi. Phasellus semper lacus et mi sodales hendrerit. Cras nisi ex, sagittis et ligula vel, ultrices hendrerit velit. Morbi sollicitudin blandit nisl sed hendrerit. Cras fringilla erat sed sollicitudin placerat. Morbi sit amet sapien rhoncus, molestie risus et, semper sem. Cras varius sem at dolor tristique, vitae dapibus velit vehicula. Pellentesque gravida commodo leo, consectetur ultrices quam vehicula et. Mauris eget neque nulla.</Typography>
+
     </>
 } 
