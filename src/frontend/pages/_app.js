ed068b0598c6d41d0e5b811808ff33cd86852676
James Grieve <jgrieve@ualberta.ca>
2023-05-30 19:41:11 -0600

    Added two files to fix VSCode ESLint bug;

--- src/frontend/pages/_app.js
+++ src/frontend/pages/_app.js
@@ -1,5 +1,19 @@
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+    <h1>App</h1>
+    <nav>
+      <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/subdirectory/example">Example</a></li>
+      </ul>
+    </nav>
+    <main>
+    <Component {...pageProps} />
+    </main>
+    <footer>
+      &copy; 2023
+    </footer>
+  </> 
 }
