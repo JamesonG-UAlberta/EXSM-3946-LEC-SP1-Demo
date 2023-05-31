6914784ad84b526c366501730e20d4a5417e0e7c
James Grieve <jgrieve@ualberta.ca>
2023-05-30 19:52:17 -0600

    MUI setup;

--- src/frontend/pages/_app.js
+++ src/frontend/pages/_app.js
@@ -1,19 +1,21 @@
 import '@/styles/globals.css'
+import { createTheme, CssBaseline, ThemeProvider, AppBar, Typography, Box, Container } from '@mui/material'
 
 export default function App({ Component, pageProps }) {
   return <>
-    <h1>App</h1>
-    <nav>
-      <ul>
-        <li><a href="/">Home</a></li>
-        <li><a href="/subdirectory/example">Example</a></li>
-      </ul>
-    </nav>
-    <main>
-    <Component {...pageProps} />
-    </main>
-    <footer>
-      &copy; 2023
-    </footer>
+    <ThemeProvider theme={createTheme()}>
+      <CssBaseline />
+      <Box>
+        <AppBar position="static">
+          <Typography variant="h3" align="center" component="h1" sx={{paddingY: "1rem"}}>Example App</Typography>
+        </AppBar>
+      </Box>
+      <Container>
+        <Component {...pageProps} />
+      </Container>
+      <Box>
+        <Typography variant="body1" align="center" component="p" sx={{paddingY: "0.5rem"}}>Footer</Typography>
+      </Box>
+    </ThemeProvider>
   </> 
 }
