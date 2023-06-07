48c32c4763b205b7befb063da951c1d01ef80c15
James Grieve <jgrieve@ualberta.ca>
2023-06-06 19:51:31 -0600

    Client data fetching;

---
+++ src/frontend/pages/fromapi.js
@@ -0,0 +1,31 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import styles from '@/styles/Home.module.css'
+import { Typography } from '@mui/material'
+import { useEffect } from 'react'
+import { useState } from 'react'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+    const [data, setData] = useState(null)
+    // Client Data Fetching
+    useEffect(() => {
+        fetch('/api')
+            .then(res => res.text())
+            .then(data => setData(data))
+    }, []);
+
+  return (
+    <>
+      <Typography variant="h1" align="center" component="h2">Example App Home</Typography>
+      <Typography variant="body1" align="center" component="p">
+        {data}
+    </Typography>
+      <Typography variant="body1" align="center" component="p">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras porttitor laoreet lobortis. Sed imperdiet quis leo et tincidunt. Sed non ante nisi. Phasellus semper lacus et mi sodales hendrerit. Cras nisi ex, sagittis et ligula vel, ultrices hendrerit velit. Morbi sollicitudin blandit nisl sed hendrerit. Cras fringilla erat sed sollicitudin placerat. Morbi sit amet sapien rhoncus, molestie risus et, semper sem. Cras varius sem at dolor tristique, vitae dapibus velit vehicula. Pellentesque gravida commodo leo, consectetur ultrices quam vehicula et. Mauris eget neque nulla.
+    </Typography>
+    </>
+  )
+}
