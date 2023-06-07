a99eb243f7113d673864a9b0f75a8fd1d928afa1
James Grieve <jgrieve@ualberta.ca>
2023-06-06 20:16:22 -0600

    Server-side rendering weather;

--- src/frontend/pages/fromapi.js
+++ src/frontend/pages/fromapi.js
@@ -5,6 +5,8 @@ import styles from '@/styles/Home.module.css'
 import { Typography } from '@mui/material'
 import { useEffect } from 'react'
 import { useState } from 'react'
+import axios from 'axios';
+import https from 'https';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -20,12 +22,11 @@ export default function Home(props) {
   return (
     <>
       <Typography variant="h1" align="center" component="h2">Example App Home</Typography>
-      <Typography variant="body1" align="center" component="p">
-       Client-Side {data}
-    </Typography>
-      <Typography variant="body1" align="center" component="p">
-       Server-Side {props.ssrBlurb}
-    </Typography>
+      <ul>
+        {props.weather.map(weather => 
+            <li>On {weather.date}, the temperature is {weather.temperatureC} degrees celcius, {weather.summary}.</li>
+        )}
+        </ul>
       <Typography variant="body1" align="center" component="p">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras porttitor laoreet lobortis. Sed imperdiet quis leo et tincidunt. Sed non ante nisi. Phasellus semper lacus et mi sodales hendrerit. Cras nisi ex, sagittis et ligula vel, ultrices hendrerit velit. Morbi sollicitudin blandit nisl sed hendrerit. Cras fringilla erat sed sollicitudin placerat. Morbi sit amet sapien rhoncus, molestie risus et, semper sem. Cras varius sem at dolor tristique, vitae dapibus velit vehicula. Pellentesque gravida commodo leo, consectetur ultrices quam vehicula et. Mauris eget neque nulla.
     </Typography>
@@ -35,10 +36,18 @@ export default function Home(props) {
 // The presence of this function converts this component (page) to server-side rendering.
 export async function getServerSideProps() {
     // Server Data Fetching
-    const ssrBlurb = await (fetch('http://localhost:3000/api').then(res => res.text()));
+    // Because the server doesn't have SSL certificates, we need to disable SSL verification.
+    const failSafeAxios = axios.create({
+        httpsAgent: new https.Agent({
+            rejectUnauthorized: false
+        })
+    });
+    // Get our data from the server.
+    const weather = await (failSafeAxios.get('https://localhost:4000/WeatherForecast'));
+    // Send the data to the page.
     return { 
         props: { 
-            ssrBlurb: ssrBlurb 
+            weather: weather.data
         } 
     }
 }
