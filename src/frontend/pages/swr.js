3ee748a8bbd190545ff8c53fe2ab12123d488db9
James Grieve <jgrieve@ualberta.ca>
2023-06-13 18:55:37 -0600

    Added SWR page and import;

---
+++ src/frontend/pages/swr.js
@@ -0,0 +1,13 @@
+import { Typography } from '@mui/material'
+import { useSWR } from 'swr'
+import axios from 'axios';
+
+export default function SWR() {
+  
+
+  return (
+    <>
+      <Typography variant="h1" align="center" component="h2">SWR Example</Typography>
+    </>
+  );
+}
