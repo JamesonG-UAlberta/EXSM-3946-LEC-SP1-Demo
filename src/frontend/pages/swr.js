6f67580284ad308722fbce2d0f886d5faaf1d30a
James Grieve <jgrieve@ualberta.ca>
2023-06-13 18:57:34 -0600

    Destructuring;

--- src/frontend/pages/swr.js
+++ src/frontend/pages/swr.js
@@ -3,7 +3,19 @@ import { useSWR } from 'swr'
 import axios from 'axios';
 
 export default function SWR() {
-  
+  // Destructuring allows us to pull variables out of an object.
+  // In this case the constants data, error, and isLoading are pulled out of the object properties returned by useSWR.
+  // Alternatively, we would have to have done something like this:
+  // const swr = useSWR('time', async () => {
+  //    // Fetcher body.
+  // });
+  // const data = swr.data;
+  // const error = swr.error;
+  // const isLoading = swr.isLoading;
+
+  const {data, error, isLoading} = useSWR('time', async () => {
+
+  });
 
   return (
     <>
