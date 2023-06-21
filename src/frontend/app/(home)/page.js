ca0816e61258624a86d95c3d0a146ac45342b556
James Grieve <jgrieve@ualberta.ca>
2023-06-20 19:39:01 -0600

    Test on other PC;

---
+++ src/frontend/app/(home)/page.js
@@ -0,0 +1,12 @@
+import { Typography } from '@mui/material'
+
+
+export default function Home() {
+  return (
+    <>
+      <Typography variant="h1" align="center" component="h2">Example App Home</Typography>
+      <Typography variant="body1" align="center" component="p">
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras porttitor laoreet lobortis. Sed imperdiet quis leo et tincidunt. Sed non ante nisi. Phasellus semper lacus et mi sodales hendrerit. Cras nisi ex, sagittis et ligula vel, ultrices hendrerit velit. Morbi sollicitudin blandit nisl sed hendrerit. Cras fringilla erat sed sollicitudin placerat. Morbi sit amet sapien rhoncus, molestie risus et, semper sem. Cras varius sem at dolor tristique, vitae dapibus velit vehicula. Pellentesque gravida commodo leo, consectetur ultrices quam vehicula et. Mauris eget neque nulla.</Typography>
+    </>
+  )
+}
